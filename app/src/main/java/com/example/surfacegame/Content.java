58360348539de4baaa75e10f036181a9c4664a41
Nikita <naadonin1337@gmail.com>
2021-03-15 15:44:01 +0300

    grisha

---
+++ app/src/main/java/com/example/surfacegame/Content.java
@@ -0,0 +1,5 @@
+package com.example.surfacegame;
+
+public class Content {
+    static com.example.surfacegame.Sun sun = new com.example.surfacegame.Sun();
+}
---
+++ app/src/main/java/com/example/surfacegame/Sun.java
@@ -0,0 +1,10 @@
+package com.example.surfacegame;
+public class Sun {
+    float x = -100f;
+    float y = 1000f;
+    float r = 100f;
+    float center_x = x-r,
+            center_y = y-r;
+    boolean open = true;
+    int col = 5;
+}
